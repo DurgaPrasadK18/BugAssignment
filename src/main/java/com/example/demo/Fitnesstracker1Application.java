d76626565843ad33f20f1ea5ef4edf967d66d87e
DurgaPrasadK18 <durgaprasadkdp18@gmail.com>
2022-02-11 11:42:59 +0530

    first commit

---
+++ src/main/java/com/example/demo/Fitnesstracker1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fitnesstracker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fitnesstracker1Application.class, args);
+	}
+
+}
